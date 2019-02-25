4b569d90fc0f3bf1d0aefaeff73f7d4095b77532
Adora Goh <heyadora@gmail.com>
2019-02-25 16:46:17 +1100

    Complete setup and creating User, Booking and Tour models

---
+++ db/migrate/20190225054158_create_tours.rb
@@ -0,0 +1,16 @@
+class CreateTours < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tours do |t|
+      t.string :title
+      t.string :location
+      t.text :description
+      t.integer :price
+      t.string :photo
+      t.references :user, foreign_key: true
+      t.date :date
+      t.integer :capacity
+
+      t.timestamps
+    end
+  end
+end

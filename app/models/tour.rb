4846d5821a9f287ae244c6a541b05c0904bc7074
Winson Tan <winsont89@gmail.com>
2019-02-28 23:06:34 +1100

    Styled the My Bookings page

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,7 @@
 class Tour < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :users, through: :bookings
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
